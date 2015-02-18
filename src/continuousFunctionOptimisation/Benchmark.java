11d728d516c01cfd9e0769f4f5454db28239df0a
BrunoGrisci <bigrisci@gmail.com>
2015-02-18 00:21:04 +0000

    Fixed bug in 15 rule, automated for generating all the output files.

--- src/continuousFunctionOptimisation/Benchmark.java
+++ src/continuousFunctionOptimisation/Benchmark.java
@@ -1,10 +1,10 @@
 package continuousFunctionOptimisation;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.Random;
-
 
 public class Benchmark {
 	
@@ -14,45 +14,95 @@ public class Benchmark {
 	static double rangeMin = -100.0;
 	static double rangeMax = 100.0;
 	static double[] generationScore;
-	static int numberOfGenerations = 5000;
+	static final int numberOfGenerations = 5000;
+	static final int numberOfExperiments = 30;
+	static final int numberOfFunctions = 10;
 	
-	//
+	static final int UNIFORM_MUTATION = 0;
+	static final int NON_UNIFORM_MUTATION_0_05 = 1;
+	static final int NON_UNIFORM_MUTATION_1 = 2;
+	static final int NON_UNIFORM_MUTATION_10 = 3;
+	static final int NON_UNIFORM_MUTATION_20 = 4;
+	static final int GAUSSIAN_MUTATION_0_05 = 5;
+	static final int GAUSSIAN_MUTATION_0_5 = 6;
+	static final int GAUSSIAN_MUTATION_1 = 7;
+	static final int GAUSSIAN_MUTATION_10 = 8;
+	static final int GAUSSIAN__MUTATION_1_5_RULE = 9;
+	
 	
 
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
 		// TODO Auto-generated method stub
-		individual = new double[numberOfGenes];
-		generationScore = new double[numberOfGenerations];
 		
-		evolutionStrategy();
+		String dirName = "Output/";
+		new File(dirName).mkdirs();
 		
-		for (double gene : individual) {
-			System.out.println(gene);
+		for (int i = 0; i < numberOfFunctions; i++) {
+			for (int j = 0; j < numberOfExperiments; j++) {
+				individual = new double[numberOfGenes];
+				generationScore = new double[numberOfGenerations];
+		
+				String fileName = dirName + Integer.toString(i) + "_" + Integer.toString(j) + ".txt";
+				evolutionStrategy(fileName, i);
+		
+				//for (double gene : individual) {
+					//System.out.println(gene);
+				//}
+				System.out.println(sphere(individual));
+			}
+		
+			System.out.println("Fim");
 		}
-		System.out.println(sphere(individual));
-		
 	}
 	
-	public static void evolutionStrategy() throws FileNotFoundException, UnsupportedEncodingException {
+	public static void evolutionStrategy(String fileName, int function) throws FileNotFoundException, UnsupportedEncodingException {
 		Random r = new Random();
-		PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
+		PrintWriter writer = new PrintWriter(fileName, "UTF-8");
 		individual = createRandomIndividual(individual);
-		int goodMutations = 0;
-		int badMutations = 0;
-		int n = 0; //LOOK IT
+		double goodMutations = 0.0;
+		double badMutations = 0.0;
+		double n = 10.0;
 		double stepSize = 1.0 + (100.0 - 1.0) * r.nextDouble();
+		
 		for (int generation = 0; generation < numberOfGenerations; generation++) {
 			offspring = new double[numberOfGenes];
-			//offspring = Mutation.uniformMutation(individual.clone());
-			//offspring = Mutation.nonUniformMutation(individual.clone(), generation, 0.05);
-			//offspring = Mutation.nonUniformMutation(individual.clone(), generation, 1.0);
-			//offspring = Mutation.nonUniformMutation(individual.clone(), generation, 10.0);
-			//offspring = Mutation.nonUniformMutation(individual.clone(), generation, 20.0);
-			//offspring = Mutation.gaussianMutation(individual.clone(), 0.05);
-			//offspring = Mutation.gaussianMutation(individual.clone(), 0.5);
-			//offspring = Mutation.gaussianMutation(individual.clone(), 1.0);
-			//offspring = Mutation.gaussianMutation(individual.clone(), 10.0);
-			offspring = Mutation.gaussianMutation(individual.clone(), stepSize);
+			
+			switch(function) {
+				case UNIFORM_MUTATION:
+					offspring = Mutation.uniformMutation(individual.clone());
+					break;
+				case NON_UNIFORM_MUTATION_0_05:
+					offspring = Mutation.nonUniformMutation(individual.clone(), generation, 0.05);
+					break;
+				case NON_UNIFORM_MUTATION_1:
+					offspring = Mutation.nonUniformMutation(individual.clone(), generation, 1.0);
+					break;
+				case NON_UNIFORM_MUTATION_10:
+					offspring = Mutation.nonUniformMutation(individual.clone(), generation, 10.0);
+					break;
+				case NON_UNIFORM_MUTATION_20:
+					offspring = Mutation.nonUniformMutation(individual.clone(), generation, 20.0);
+					break;
+				case GAUSSIAN_MUTATION_0_05:
+					offspring = Mutation.gaussianMutation(individual.clone(), 0.05);
+					break;
+				case GAUSSIAN_MUTATION_0_5:
+					offspring = Mutation.gaussianMutation(individual.clone(), 0.5);
+					break;
+				case GAUSSIAN_MUTATION_1:
+					offspring = Mutation.gaussianMutation(individual.clone(), 1.0);
+					break;
+				case GAUSSIAN_MUTATION_10:
+					offspring = Mutation.gaussianMutation(individual.clone(), 10.0);
+					break;
+				case GAUSSIAN__MUTATION_1_5_RULE:
+					offspring = Mutation.gaussianMutation(individual.clone(), stepSize);
+					break;
+				default:
+					System.out.println("CODE NOT VALID");
+					break;
+			
+			}
 			if (sphere(offspring) < sphere(individual)) {
 				individual = offspring;
 				goodMutations = goodMutations + 1;
@@ -61,14 +111,16 @@ public class Benchmark {
 				badMutations = badMutations + 1;
 			}
 			if (badMutations + goodMutations == n) {
-				if (goodMutations/n > 1/5) {
-					stepSize = stepSize * 2;
+				if (goodMutations/n > (1/5)) {
+					stepSize = stepSize * 2.0;
+					//System.out.println(stepSize);
 				}
-				if (goodMutations/n < 1/5) {
-					stepSize = stepSize/2;
+				if (goodMutations/n < (1/5)) {
+					stepSize = stepSize/2.0;
+					//System.out.println(stepSize);
 				}
-				badMutations = 0;
-				goodMutations = 0;
+				badMutations = 0.0;
+				goodMutations = 0.0;
 			}
 			
 			
